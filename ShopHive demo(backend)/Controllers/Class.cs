8f5d1084302ec3d6b6ed438b45d8deb6c6de66ff
Ahmed Javed <ahmedjavedd@gmail.com>
2023-03-03 23:20:27 +0500

    Add project files.

---
+++ ShopHive demo(backend)/Controllers/Class.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace ShopHive_demo_backend_.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class SearchController : ControllerBase
+    
+        private const string ShophiveApiEndpoint = "https://www.shophive.com/magebig_ajaxsearch/ajax/index/?q={0}";
+
+        [HttpGet("{query}")]
+        public async Task<IEnumerable<Model.Product>> Search(string query)
+        {
+            using (var httpClient = new HttpClient())
+            {
+                var apiUrl = string.Format(ShophiveApiEndpoint, query);
+                var response = await httpClient.GetAsync(apiUrl);
+                var content = await response.Content.ReadAsStringAsync();
+                var products = JsonConvert.DeserializeObject<IEnumerable<Model.Product>>(content);
+                return products;
+            }
+        }
+    }
+}
---
+++ ShopHive demo(backend)/Model/Product.cs
@@ -0,0 +1,12 @@
+﻿namespace ShopHive_demo_backend_.Model
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string name { get; set; }
+        public string image { get; set; }
+        public string? review_rating { get; set; }
+        public string price { get; set;}
+        public string url { get; set; }
+    }
+}
